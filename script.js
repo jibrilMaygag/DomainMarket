b82b0be68cb6db2de9ab895ab79418e4934e0a9f
jibrilMaygag <jibrilmeygag@gmail.com>
2025-04-27 00:21:13 +0300

    make ready for production

--- script.js
+++
@@ -1,6 +0,0 @@
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    item.classList.toggle("collapsible--expanded");
-  })
-);
